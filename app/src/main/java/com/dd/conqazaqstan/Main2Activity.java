4f44844a81522d884de6e8d3beca746fb02e4346
Daniyar <dds861@gmail.com>
2020-04-26 05:08:52 +0200

    Ema Added

--- app/src/main/java/com/dd/conqazaqstan/Main2Activity.java
+++ app/src/main/java/com/dd/conqazaqstan/Main2Activity.java
@@ -1,7 +1,7 @@
 package com.dd.conqazaqstan;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ListView;
 
 import com.dd.conqazaqstan.departments.Department;
---
+++ app2/src/main/java/com/dd/conqazaqstan/sqlite/ui/category/CategoryViewFragment.kt
@@ -0,0 +1,86 @@
+package com.dd.conqazaqstan.sqlite.ui.category
+
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.carmabs.ema.core.constants.STRING_EMPTY
+import com.carmabs.ema.core.state.EmaExtraData
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.dd.conqazaqstan.sqlite.R
+import com.dd.conqazaqstan.sqlite.base.BaseToolbarsFragment
+import com.dd.conqazaqstan.sqlite.ui.main.MainToolbarsViewModel
+import kotlinx.android.synthetic.main.fragment_category.*
+import org.kodein.di.generic.instance
+
+class CategoryViewFragment : BaseToolbarsFragment<CategoryState, CategoryViewModel, CategoryNavigator.Navigation>() {
+    /**
+     * Default variables
+     */
+    override val layoutId: Int = R.layout.fragment_category
+    override val navigator: CategoryNavigator by instance()
+    override val viewModelSeed: CategoryViewModel by instance()
+
+    /**
+     * Custom variables
+     */
+    private lateinit var vm: CategoryViewModel
+
+    /**
+     * Default functions
+     */
+    override fun onInitializedWithToolbarsManagement(viewModel: CategoryViewModel, mainToolbarViewModel: MainToolbarsViewModel) {
+        vm = viewModel
+        setupRecycler()
+        setupListeners()
+    }
+
+    override fun onSingleEvent(data: EmaExtraData) {
+    }
+
+    override fun onNormal(data: CategoryState) {
+        loadRecyclerViews(data)
+    }
+
+    override fun onAlternative(data: EmaExtraData) {
+    }
+
+    override fun onError(error: Throwable) {
+    }
+
+    override fun onSingle(data: EmaExtraData) {
+    }
+
+    /**
+     * Custom functions
+     */
+    private fun setupRecycler() {
+        rvCategory.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+    }
+
+    private fun loadRecyclerViews(data: CategoryState) {
+        rvCategory.adapter = data.listCategories.toMutableList().let { it ->
+            CategoryAdapter(listItems = it) {
+                vm.onActionCategoryClick(it)
+            }
+        }
+    }
+
+    private fun setupListeners() {
+        et_search.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun afterTextChanged(editable: Editable) {
+                vm.onActionSearch(editable.toString())
+            }
+        })
+
+        iv_category_clear_search.setOnClickListener {
+            YoYo.with(Techniques.FadeOut).duration(150).repeat(0).playOn(it)
+            YoYo.with(Techniques.FadeIn).duration(350).repeat(0).playOn(it)
+
+            et_search.setText(STRING_EMPTY)
+        }
+    }
+}
