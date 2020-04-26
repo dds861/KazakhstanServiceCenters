3fa2a43be5861fdf30f7a916ac84332efe7ed234
Daniyar <dds861@gmail.com>
2020-04-26 15:25:55 +0200

    Redesign layout

--- app2/src/main/java/com/dd/conqazaqstan/sqlite/ui/category/CategoryViewFragment.kt
+++ app2/src/main/java/com/dd/conqazaqstan/sqlite/ui/category/CategoryViewFragment.kt
@@ -1,13 +1,8 @@
 package com.dd.conqazaqstan.sqlite.ui.category
 
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.carmabs.ema.core.constants.STRING_EMPTY
 import com.carmabs.ema.core.state.EmaExtraData
-import com.daimajia.androidanimations.library.Techniques
-import com.daimajia.androidanimations.library.YoYo
 import com.dd.conqazaqstan.sqlite.R
 import com.dd.conqazaqstan.sqlite.base.BaseToolbarsFragment
 import com.dd.conqazaqstan.sqlite.ui.main.MainToolbarsViewModel
@@ -33,7 +28,6 @@ class CategoryViewFragment : BaseToolbarsFragment<CategoryState, CategoryViewMod
     override fun onInitializedWithToolbarsManagement(viewModel: CategoryViewModel, mainToolbarViewModel: MainToolbarsViewModel) {
         vm = viewModel
         setupRecycler()
-        setupListeners()
     }
 
     override fun onSingleEvent(data: EmaExtraData) {
@@ -64,23 +58,6 @@ class CategoryViewFragment : BaseToolbarsFragment<CategoryState, CategoryViewMod
             CategoryAdapter(listItems = it) {
                 vm.onActionCategoryClick(it)
             }
-        }
-    }
-
-    private fun setupListeners() {
-        et_search.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
-            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
-            override fun afterTextChanged(editable: Editable) {
-                vm.onActionSearch(editable.toString())
-            }
-        })
-
-        iv_category_clear_search.setOnClickListener {
-            YoYo.with(Techniques.FadeOut).duration(150).repeat(0).playOn(it)
-            YoYo.with(Techniques.FadeIn).duration(350).repeat(0).playOn(it)
-
-            et_search.setText(STRING_EMPTY)
         }
     }
 }
